d935bcc1300f0146d3f220e078a8d2f78b35df2d
iamlydial <lydia.lavecchia@gmail.com>
2024-09-11 12:21:14 +0200

    add Java comments to CustomFieldLabel and FieldLabelFactory

--- src/bank/management/system/customfields/CustomFieldLabel.java
+++ src/bank/management/system/customfields/CustomFieldLabel.java
@@ -3,11 +3,27 @@ package bank.management.system.customfields;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * CustomFieldLabel is a custom class that extends JLabel to provide
+ * additional styling and positioning capabilities.
+ */
 public class CustomFieldLabel extends JLabel {
+
+    /**
+     * Constructs a CustomFieldLabel with specified properties.
+     *
+     * @param text   The text to be displayed on the label.
+     * @param color  The color of the text.
+     * @param font   The font used for the text.
+     * @param x      The x-coordinate for the label's position.
+     * @param y      The y-coordinate for the label's position.
+     * @param width  The width of the label.
+     * @param height The height of the label.
+     */
     public CustomFieldLabel(String text, Color color, Font font, int x, int y, int width, int height) {
-        super(text);
-        setForeground(color);
-        setFont(font);
-        setBounds(x, y, width, height);
+        super(text); // Set the text for the label
+        setForeground(color); // Set the text color
+        setFont(font); // Set the font style and size
+        setBounds(x, y, width, height); // Set the position and size of the label
     }
 }
--- src/bank/management/system/factories/FieldLabelFactory.java
+++ src/bank/management/system/factories/FieldLabelFactory.java
@@ -1,12 +1,27 @@
 package bank.management.system.factories;
 
 import bank.management.system.customfields.CustomFieldLabel;
-import bank.management.system.customfields.CustomTitleLabel;
 
 import java.awt.*;
 
+/**
+ * FieldLabelFactory is a factory class for creating instances of CustomFieldLabel.
+ * This factory is used to encapsulate the creation logic for field labels.
+ */
 public class FieldLabelFactory {
+
+    /**
+     * Creates a CustomFieldLabel instance with predefined styles and position.
+     *
+     * @param text   The text to be displayed on the label.
+     * @param x      The x-coordinate for the label's position.
+     * @param y      The y-coordinate for the label's position.
+     * @param width  The width of the label.
+     * @param height The height of the label.
+     * @return A CustomFieldLabel instance with the specified properties.
+     */
     public static CustomFieldLabel createTitleLabel(String text, int x, int y, int width, int height) {
+        // Create and return a CustomFieldLabel with predefined color, font, and position
         return new CustomFieldLabel(text, Color.WHITE, new Font("Railway", Font.BOLD, 28), x, y, width, height);
     }
 }
