005f2447594a0ffbdb99b8b636e92172f298f67f
iamlydial <lydia.lavecchia@gmail.com>
2024-09-16 10:33:16 +0200

    add Signup class
    - add new factory method to customize color on titles factory
    - add icon and title to signup class

---
+++ src/bank/management/system/Signup.java
@@ -0,0 +1,44 @@
+package bank.management.system;
+
+import bank.management.system.customfields.CustomFieldLabel;
+import bank.management.system.customfields.CustomTitleLabel;
+import bank.management.system.factories.TitleLabelFactory;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+public class Signup extends JFrame {
+    Random ran = new Random();
+    long first4 = (ran.nextLong()%9000L)+1000L;
+    String first = " " + Math.abs(first4);
+    Signup(){
+
+        super("APPLICATION FORM");
+
+        // Load an image resource using the system's class loader and create an ImageIcon from it
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/bank.png"));
+        // Get the image from the ImageIcon and scale it to 100x100 pixels, maintaining the default scaling quality
+        Image i2 = i1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
+        // Create a new ImageIcon from the scaled image
+        ImageIcon i3 = new ImageIcon(i2);
+        // Create a JLabel to display the ImageIcon
+        JLabel image = new JLabel(i3);
+        // Set the position and size of the JLabel on the container (x = 350, y = 10, width = 100, height = 100)
+        image.setBounds(25, 10, 100, 100);
+        // Add the JLabel with the image to the JFrame container
+        add(image);
+
+        CustomTitleLabel label1 = TitleLabelFactory.createTitleLabelCustomColor("APPLICATION FORM NO." + first, Color.BLACK,160, 20, 600, 40);
+        add(label1);
+
+        getContentPane().setBackground(new Color(222,255, 228));
+        setLayout(null);
+        setSize(850, 800);
+        setLocation(360, 40);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new Signup();
+    }
+}
--- src/bank/management/system/factories/TitleLabelFactory.java
+++ src/bank/management/system/factories/TitleLabelFactory.java
@@ -7,5 +7,9 @@ public class TitleLabelFactory {
     public static CustomTitleLabel createTitleLabel(String text, int x, int y, int width, int height) {
         return new CustomTitleLabel(text, Color.WHITE, new Font("AvantGarde", Font.BOLD, 38), x, y, width, height);
     }
+
+    public static CustomTitleLabel createTitleLabelCustomColor(String text, Color color,  int x, int y, int width, int height) {
+        return new CustomTitleLabel(text, color, new Font("AvantGarde", Font.BOLD, 38), x, y, width, height);
+    }
 }
 
